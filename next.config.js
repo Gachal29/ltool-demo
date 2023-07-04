0e61c9d2a4aa2ef4f89b555afa5114b6e05c263b
Gachal29 <taisuke0129t@gmail.com>
2023-07-04 13:31:25 +0900

    code base

--- next.config.js
+++ next.config.js
@@ -1,4 +1,31 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/favicon.ico",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=86400"
+          }
+        ]
+      }
+    ]
+  },  
+  async headers() {
+    return [
+      {
+        source: "/",
+        headers: [
+          {
+            key: "Link",
+            value: "/favicons/favicon.ico; rel=icon"
+          }
+        ]
+      }
+    ]
+  }
+}
 
 module.exports = nextConfig
