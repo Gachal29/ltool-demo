025057e256bb1a030b174402b7fdd1aacb93d2d7
Gachal29 <taisuke0129t@gmail.com>
2023-08-07 06:46:58 +0900

    トップページの修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,36 @@
+"use client"
+
 import PresentationStart from "components/PresentationStart"
 import { NextPage } from "next"
+import Image from "next/image"
+import GeneralView from "@/site-materials/general-view.png"
+import RightArrow from "@/site-materials/right-arrow.png"
+import { useWindowSize } from "utils/useWindowSize"
 
 
 const Home: NextPage = () => {
+  const windowSize = useWindowSize()
+
   return (
     <main>
-      <h1 className="text-4xl font-bold">LTooL</h1>
+      <h1 className="ml-2 text-4xl font-bold">LTooL</h1>
       <div className="my-2 mx-4">
-        <h2 className="text-2xl text-center mb-2">
+        <h2 className="text-4xl text-center mb-2">
           <u>LTooLとは</u>
         </h2>
-        <span className="w-full text-xl">
-          LTooLの説明
-        </span>
+        <div className="w-full text-xl">
+          <p className="text-3xl my-4">資料を作りながら話すプレゼンテーションツール</p>
+          <Image
+            className="mx-auto"
+            src={GeneralView}
+            alt="概要図"
+            width={windowSize.width/2} />
+        </div>
+      </div>
+
+      <div className="fixed bottom-6 right-24 flex items-center">
+        <p className="font-bold text-2xl mr-4">ここからプレゼンテーションを体験してください</p>
+        <Image src={RightArrow} alt="" width={50} />
       </div>
 
       <PresentationStart />
